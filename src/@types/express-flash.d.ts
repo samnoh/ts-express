d46ed149a6b5d7d97383aa9ebf5415e648916151
Sam Noh <samnoh21@outlook.com>
2019-08-09 18:05:56 +1200

    mod - express-flash type

--- src/@types/express-flash.d.ts
+++ src/@types/express-flash.d.ts
@@ -3,11 +3,9 @@
 type flashInput = { [key in 'msg']: string };
 
 declare namespace Express {
-    export interface Request extends flashInterface {}
-}
-
-interface flashInterface {
-    flash(event: string, message: flashInput | flashInput[]): void;
+    export interface Request {
+        flash(event: string, message: flashInput | flashInput[]): void;
+    }
 }
 
 declare module 'express-flash';
